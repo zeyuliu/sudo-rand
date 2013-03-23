7700b9ef6ae0a80e4cb4cb811ed4148de4baaa5f
Andrew <zeyuliu@gmail.com>
2013-03-22 18:29:13 -0700

    20

--- sudoku3/sudoku3/SudokuView.m
+++ sudoku3/sudoku3/SudokuView.m
@@ -9,6 +9,48 @@
 #import "SudokuView.h"
 #import "SudokuWindowController.h" //14
 
+@interface SudokuView (hidden) //19
+-(NSRect)drawHashInBounds: (NSRect)bounds usingColor:(NSColor*) color;//19
+
+@end
+
+@implementation SudokuView (hidden) //19
+-(NSRect)drawHashInBounds: (NSRect)bounds usingColor:(NSColor*) color //19
+{
+    //Actual implementation done in 20
+    CGFloat top = bounds.origin.y;
+    CGFloat bottom = top + bounds.size.height;
+    CGFloat left = bounds.origin.x;
+    CGFloat right = left + bounds.size.width;
+    
+    CGFloat thirdWidth = bounds.size.width / 3.0;
+    CGFloat thirdHeight = bounds.size.height / 3.0;
+    CGFloat lineWidth = ( thirdWidth > thirdHeight ) ? thirdWidth / 30.0 : thirdHeight / 30.0;
+    
+    NSBezierPath* framePath = [NSBezierPath bezierPath];
+    
+    [framePath moveToPoint: NSMakePoint(left + thirdWidth,top)];
+    [framePath lineToPoint: NSMakePoint(left + thirdWidth,bottom)];
+    [framePath moveToPoint: NSMakePoint(right - thirdWidth,top)];
+    [framePath lineToPoint: NSMakePoint(right - thirdWidth,bottom)];
+    [framePath moveToPoint: NSMakePoint(left,top + thirdHeight)];
+    [framePath lineToPoint: NSMakePoint(right,top + thirdHeight)];
+    [framePath moveToPoint: NSMakePoint(left,bottom - thirdHeight)];
+    [framePath lineToPoint: NSMakePoint(right,bottom - thirdHeight)];
+    
+    [color setStroke];
+    
+    [framePath setLineWidth: lineWidth];
+    
+    [framePath stroke];
+    
+    return( NSMakeRect( bounds.origin.x, bounds.origin.y, thirdWidth, thirdHeight ) );
+}
+
+
+@end
+
+
 @implementation SudokuView
 @synthesize _windowController; //14
 
@@ -22,9 +64,9 @@
     return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect
+- (void)drawRect:(NSRect)dirtyRect //19
 {
-    // Drawing code here.
+    NSRect oneSqureBounds = [self drawHashInBounds:self.bounds usingColor:[NSColor blackColor]]; // 20
 }
 
 @end
