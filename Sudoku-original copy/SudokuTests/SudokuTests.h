0b8f29e1676ac28c0aa11d0742d4df7ff296e705
Matthew Miller <mattmiller@Matthews-MacBook-Air.local>
2013-03-22 18:32:10 -0700

    Duplicate Original for working backwards

---
+++ Sudoku-original copy/SudokuTests/SudokuTests.h
@@ -0,0 +1,13 @@
+//
+//  SudokuTests.h
+//  SudokuTests
+//
+//  Created by Glenn Sugden on 2011.06.06.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SudokuTests : SenTestCase
+
+@end
